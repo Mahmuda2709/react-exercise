90ac03ef1446de6bf391f95859a0a633db430298
Mahmuda <mahmuda2709@gmail.com>
2021-12-16 20:44:35 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let[count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit c7 fun <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        {count}
+        <button
+        onClick={() =>{
+          setCount(count +1);
+          console.log('CLICKED! ${count}');
+        }}
         >
-          Learn React
-        </a>
+          click me
+        </button>
       </header>
     </div>
   );
